d97ea2bb1c0539325092b26587ff22c58ad3dbc8
Alexandra <127262337+pcalexandrat@users.noreply.github.com>
2025-09-14 16:16:20 -0300

    ,

--- script2.js
+++ script2.js
@@ -12,16 +12,15 @@ function mostraPalavrasChave() {
 function processaTexto(texto) {
    
   let palavras = texto.split(/\P{L}+/u);
-let frequencias = []; 
-for (let i in palavras) {
-    frequencias[i] = 0;
-  
+let frequencias = {}; 
+for (let i of palavras) {
+  frequencias[i] = 0;
 
-  for (let j in palavras) {
-  if (palavras[i] == palavras[j]) {
-    frequencias[i]++;
+  for (let j of palavras) {
+    if (i == j) {
+      frequencias[i]++;
+    }
   }
-}
 }
   console.log(frequencias);
 
