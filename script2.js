f4e1b13e6e84e74980cd5a9657d4a41f7bbece4b
Alexandra <127262337+pcalexandrat@users.noreply.github.com>
2025-09-14 17:10:00 -0300

    .

--- script2.js
+++ script2.js
@@ -11,9 +11,150 @@ function mostraPalavrasChave() {
 
   campoResultado.textContent = palavrasChave.join(", ");
 }
+function tiraPalavrasRuins(texto) {
+    const PALAVRAS_RUINS = new Set([const PALAVRAS_RUINS = new Set([
+    "que",
+    "para",
+    "com",
+    "não",
+    "uma",
+    "por",
+    "mais",
+    "dos",
+    "como",
+    "mas",
+    "foi",
+    "ele",
+    "das",
+    "tem",
+    "seu",
+    "sua",
+    "ser",
+    "quando",
+    "muito",
+    "está",
+    "também",
+    "pelo",
+    "pela",
+    "até",
+    "isso",
+    "ela",
+    "entre",
+    "era",
+    "depois",
+    "sem",
+    "mesmo",
+    "aos",
+    "ter",
+    "seus",
+    "quem",
+    "nas",
+    "esse",
+    "eles",
+    "estão",
+    "você",
+    "tinha",
+    "foram",
+    "essa",
+    "num",
+    "nem",
+    "suas",
+    "meu",
+    "minha",
+    "têm",
+    "numa",
+    "pelos",
+    "elas",
+    "havia",
+    "seja",
+    "qual",
+    "será",
+    "nós",
+    "tenho",
+    "lhe",
+    "deles",
+    "essas",
+    "esses",
+    "pelas",
+    "este",
+    "fosse",
+    "dele",
+    "vocês",
+    "vos",
+    "lhes",
+    "meus",
+    "minhas",
+    "teu",
+    "tua",
+    "teus",
+    "tuas",
+    "nosso",
+    "nossa",
+    "nossos",
+    "nossas",
+    "dela",
+    "delas",
+    "esta",
+    "estes",
+    "estas",
+    "aquele",
+    "aquela",
+    "aqueles",
+    "aquelas",
+    "isto",
+    "aquilo",
+    "estou",
+    "estamos",
+    "estive",
+    "esteve",
+    "estivemos",
+    "estiveram",
+    "estava",
+    "estávamos",
+    "estavam",
+    "e",
+    "ou",
+    "onde",
+    "aquilo",
+    "embora",
+    "apesar",
+    "porque",
+    "enquanto",
+    "contudo",
+    "entretanto",
+    "portanto",
+    "além",
+    "antes",
+    "todavia",
+    "inclusive",
+    "diante",
+    "sobre",
+    "quanto",
+    "apenas",
+    "desde",
+    "uma vez",
+    "segundo",
+    "junto"
+]);
+    const palavrasBoas = [];
+
+  for (let palavra of palavras) {
+    if (!PALAVRAS_RUINS.has(palavra) && palavra.length > 2) {
+      palavrasBoas.push(palavra);
+    }
+  }
+
+  return palavrasBoas;
+
+}
 
 function processaTexto(texto) {
   let palavras = texto.split(/\P{L}+/u);
+
+  for (let i in palavras) {
+  palavras[i] = palavras[i].toLowerCase();
+  palavras = tiraPalavrasRuins(palavras);
+}
 
   const frequencias = contaFrequencias(palavras);
 
