92c0adf4a92f13f1a1b81dc3005038a9968fe8af
Alexandra <127262337+pcalexandrat@users.noreply.github.com>
2025-09-14 17:14:41 -0300

    .

--- script2.js
+++ script2.js
@@ -1,3 +1,4 @@
+import { PALAVRAS_RUINS } from "./palavrasRuins.js";
 const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
 
 botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
